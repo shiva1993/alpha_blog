36873a8e33f872ee0a06d233b85e0717962fa242
checkout <shiva1993>
2016-03-24 17:21:28 +0530

    added validtions for user email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-
+	validates :username,presence: true,uniqueness: { case_sensitive: false},
+			   length: { minimum: 5, maximum: 10}
+	validates :email,presence: true,length: { maximum: 105},
+			   uniqueness: { case_sensitive: false},
+			   format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
 end
