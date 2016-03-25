a5fd29014a0303a5406b8c29745cac1f1630c5af
checkout <shiva1993>
2016-03-25 11:19:53 +0530

    assocations with articles and user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true,length: { minimum: 5, maximum: 10}
-	validates :description,presence: true,length: { minimum: 15, maximum: 100} 
 	belongs_to :user
+	validates :title, presence: true,uniqueness: { case_sensitive: false},length: { minimum: 5, maximum: 10}
+	validates :description,presence: true,length: { minimum: 15, maximum: 100} 
+	validates :user_id,presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-	validates :username,presence: true,uniqueness: { case_sensitive: false},
+	has_many :articles
+	before_save { self.email=email.downcase}
+	validates :username,presence: true,uniqueness:{ case_sensitive: false},
 			   length: { minimum: 5, maximum: 10}
 	validates :email,presence: true,length: { maximum: 105},
 			   uniqueness: { case_sensitive: false},
